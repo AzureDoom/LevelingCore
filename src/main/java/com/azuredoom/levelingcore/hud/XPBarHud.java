3d4ce12e56d0fa418e37439778d361107d08cac3
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-01-21 09:37:52 -0500

    Should fix crash with MHUD changes if merged

--- src/main/java/com/azuredoom/levelingcore/hud/XPBarHud.java
+++ src/main/java/com/azuredoom/levelingcore/hud/XPBarHud.java
@@ -1,7 +1,9 @@
 package com.azuredoom.levelingcore.hud;
 
+import com.hypixel.hytale.common.plugin.PluginIdentifier;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.entities.player.hud.CustomUIHud;
+import com.hypixel.hytale.server.core.plugin.PluginManager;
 import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.util.Config;
@@ -59,7 +61,10 @@ public class XPBarHud extends CustomUIHud {
                 Message.raw("LVL: " + currentLevel)
             );
         }
-        update(false, uiCommandBuilder); // false = don't clear existing UI
+        // Only update when MultipleHUD is not installed
+        if (PluginManager.get().getPlugin(new PluginIdentifier("Buuz135", "MultipleHUD")) == null) {
+            update(false, uiCommandBuilder); // false = don't clear existing UI
+        }
     }
 
     public static void updateHud(@NonNullDecl PlayerRef playerRef) {
