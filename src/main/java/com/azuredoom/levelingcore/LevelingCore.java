0dbb7063117115b03bad02cc026e96f065194844
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-03-08 19:10:36 -0400

    Adds add/remove/set stats commands

--- src/main/java/com/azuredoom/levelingcore/LevelingCore.java
+++ src/main/java/com/azuredoom/levelingcore/LevelingCore.java
@@ -25,6 +25,9 @@ import com.azuredoom.levelingcore.commands.*;
 import com.azuredoom.levelingcore.commands.level.AddLevelCommand;
 import com.azuredoom.levelingcore.commands.level.RemoveLevelCommand;
 import com.azuredoom.levelingcore.commands.level.SetLevelCommand;
+import com.azuredoom.levelingcore.commands.stats.AddStatsCommand;
+import com.azuredoom.levelingcore.commands.stats.RemoveStatsCommand;
+import com.azuredoom.levelingcore.commands.stats.SetStatsCommand;
 import com.azuredoom.levelingcore.commands.xp.AddXpCommand;
 import com.azuredoom.levelingcore.commands.xp.RemoveXpCommand;
 import com.azuredoom.levelingcore.compat.dynamictooltips.DynamicTooltipsLibCompat;
@@ -287,6 +290,9 @@ public class LevelingCore extends JavaPlugin {
         getCommandRegistry().registerCommand(new RemoveLevelCommand(config));
         getCommandRegistry().registerCommand(new RemoveXpCommand(config));
         getCommandRegistry().registerCommand(new ShowStatsCommand(config));
+        getCommandRegistry().registerCommand(new AddStatsCommand());
+        getCommandRegistry().registerCommand(new RemoveStatsCommand());
+        getCommandRegistry().registerCommand(new SetStatsCommand());
     }
 
     public void registerAllSystems() {
