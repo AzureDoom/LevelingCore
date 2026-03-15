3cc80347418cd4bec8d0e5e98622a270e8098363
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-03-15 17:20:10 -0400

    lint

--- src/main/java/com/azuredoom/levelingcore/commands/ShowStatsCommand.java
+++ src/main/java/com/azuredoom/levelingcore/commands/ShowStatsCommand.java
@@ -5,7 +5,6 @@ import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.protocol.GameMode;
 import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime;
-import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
 import com.hypixel.hytale.server.core.entity.entities.Player;
