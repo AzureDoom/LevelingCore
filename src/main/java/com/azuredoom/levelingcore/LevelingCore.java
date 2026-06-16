56702e722599e2dc9e925ee52d1dda5606844f87
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-06-16 15:17:46 -0400

    Update LevelingCore.java

--- src/main/java/com/azuredoom/levelingcore/LevelingCore.java
+++ src/main/java/com/azuredoom/levelingcore/LevelingCore.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.*;
 import javax.annotation.Nonnull;
 
 import com.azuredoom.levelingcore.commands.*;
